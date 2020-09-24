3c7e221b37606eebfef53ae419530d11d880c923
Daniele <daniele.montesi@gmail.com>
2020-09-24 16:30:51 +0200

    Upgrade to Expo 39

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from "react";
-import { createStackNavigator, createAppContainer } from "react-navigation";
 import ContextExample from './src/ContextExample';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
  
 
-const AppNavigator = createStackNavigator({
-  Home: {
-	screen: ContextExample
-  }
-});
+const Stack = createStackNavigator();
 
-export default createAppContainer(AppNavigator);
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={ContextExample}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+    return (
+      <NavigationContainer>
+        <MyStack />
+      </NavigationContainer>
+    );
+}
