6ee31eef740e4ce8991d21059599d62125798824
Andrea Borgogelli Avveduti <borgogelli@iubar.it>
2023-01-23 20:10:20 +0100

    .

--- App.js
+++ App.js
@@ -4,18 +4,15 @@ import ContextExample from './src/ContextExample';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-// const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
-// function MyStack() {
-//   return (
-//     <Stack.Navigator>
-//       <Stack.Screen
-//         name="Home"
-//         component={ContextExample}
-//       />
-//     </Stack.Navigator>
-//   );
-// }
+function MyStack() {
+	return (
+		<Stack.Navigator>
+			<Stack.Screen name="Home" component={ContextExample} />
+		</Stack.Navigator>
+	);
+}
 
 export default function App() {
 	if (false) {
